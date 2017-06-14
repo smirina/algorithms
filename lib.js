dfcd4b48227301a9310e449cf6c5470a3ade6ddb
smirina <nitchenkoiv@gmail.com>
2017-06-14 17:08:36 +0300

    Add binary search

--- lib.js
+++ lib.js
@@ -6,20 +6,38 @@ function sortby () {
 
   function linearSearch (array, key) {
     var position = 'your element is in another array'
-
     for (var i = 0; i < array.length; i++) {
-
-      if (array[i] == key) {
-        position = 'position: ' + i
+      if (array[i] === key) {
+        position = 'linear search: position: ' + i
         break;
       }
     }
+    return position
+  }
 
+  function binarySearch (array, key) {
+    var position = 'your element is in another array'
+    var n = array.length
+    var first = 0
+    var last = n
+    var mid = parseInt((first+last)/2)
+    while(array[mid] !== key && first < last) {
+      if (key < array[mid]){
+          last = mid - 1;
+      } else if (key > array[mid]){
+          first = mid + 1;
+      }
+      mid = parseInt((first+last)/2)
+    }
+    if (array[mid] === key) {
+      position = 'binary search: position: ' + mid
+    }
     return position
   }
 
   return {
     testFunc: testFunc,
-    linearSearch: linearSearch
+    linearSearch: linearSearch,
+    binarySearch: binarySearch
   }
 }
--- test.js
+++ test.js
@@ -2,9 +2,12 @@
 var sortby = sortby()
 
 var Arr = [1, 4, 19, 7, 999, 38, 2, 5, 12, 27, 50, 100]
+var sortedArr = [0, 1, 2,13, 19, 58, 99]
 
 console.log(Arr)
+console.log(sortedArr)
 
-sortby.testFunc('myData')
+sortby.testFunc('test')
 
 console.log(sortby.linearSearch(Arr, 7))
+console.log(sortby.binarySearch(sortedArr, 2))
