fa3cdd3723303f2d1484d0ab57eac489205ea01f
smirina <nitchenkoiv@gmail.com>
2017-06-14 16:01:23 +0300

    Add linear search

--- lib.js
+++ lib.js
@@ -4,7 +4,22 @@ function sortby () {
     console.log(input)
   }
 
+  function linearSearch (array, key) {
+    var position = 'your element is in another array'
+
+    for (var i = 0; i < array.length; i++) {
+
+      if (array[i] == key) {
+        position = 'position: ' + i
+        break;
+      }
+    }
+
+    return position
+  }
+
   return {
-    testFunc: testFunc
+    testFunc: testFunc,
+    linearSearch: linearSearch
   }
 }
--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 
 var sortby = sortby()
 
+var Arr = [1, 4, 19, 7, 999, 38, 2, 5, 12, 27, 50, 100]
+
+console.log(Arr)
+
 sortby.testFunc('myData')
+
+console.log(sortby.linearSearch(Arr, 7))
